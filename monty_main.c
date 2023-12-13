23701e699944b5b5cb6bceebb403cc0fc9cb593b
root <enangita@gmail.com>
2023-12-13 10:26:02 -0800

    monty

--- monty_main.c
+++ monty_main.c
@@ -1,18 +1,109 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void)
+void process_file(const char *filename);
+void execute_opcode(const char *opcode, stack_t **stack, unsigned int line_number);
+
+int main(int argc, char *argv[])
 {
-    stack_t *stack = NULL; /* Initialize an empty stack */
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-    /* Example usage: push 1, push 2, push 3, pall */
-    push(&stack, 1);
-    push(&stack, 2);
-    push(&stack, 3);
-    pall(&stack, 1); /* The line number here is just a placeholder, replace it with the actual line number */
+    process_file(argv[1]);
 
-    /* Clean up the stack if needed */
+    return EXIT_SUCCESS;
+}
 
-    return (0);
+void process_file(const char *filename)
+{
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
+    char *opcode;
+    char *arg;
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+        opcode = strtok(line, " \t\n$");  /* Include $ as a delimiter */
+
+        if (opcode != NULL && *opcode != '#')
+        {
+            /* If push, get the argument and pass it to the push function */
+            if (strcmp(opcode, "push") == 0)
+            {
+                arg = strtok(NULL, " \t\n$");
+                if (arg == NULL || !is_int(arg))
+                {
+                    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                    exit(EXIT_FAILURE);
+                }
+
+                execute_opcode(opcode, &stack, line_number);
+            }
+            else
+            {
+                execute_opcode(opcode, &stack, line_number);
+            }
+        }
+    }
+
+    free(line);
+    fclose(file);
+}
+
+void execute_opcode(const char *opcode, stack_t **stack, unsigned int line_number)
+{
+    int i = 0;
+
+    instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        /* Add other opcode-function pairs as needed */
+        {NULL, NULL} /* Terminator for the array */
+    };
+
+    while (instructions[i].opcode != NULL)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            /* If push, get the argument and pass it to the push function */
+            if (strcmp(opcode, "push") == 0)
+            {
+                char *arg = strtok(NULL, " \t\n$");
+                if (arg == NULL || !is_int(arg))
+                {
+                    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                    exit(EXIT_FAILURE);
+                }
+
+                instructions[i].f(stack, line_number);
+            }
+            else
+            {
+                instructions[i].f(stack, line_number);
+            }
+
+            return;
+        }
+        i++;
+    }
+
+    fprintf(stderr, "L%u: Unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
 }
