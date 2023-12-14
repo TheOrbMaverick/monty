8b957ccee4bfe75cfb71b69bdf11b927a57cf7eb
root <enangita@gmail.com>
2023-12-14 05:52:26 -0800

    checking

--- monty.h
+++ monty.h
@@ -35,5 +35,6 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 int is_int(const char *str);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
