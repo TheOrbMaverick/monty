67bc09b804be21684d0ce82b36a2e603394a4b18
root <enangita@gmail.com>
2023-12-14 11:42:18 -0800

    che

---
+++ monty_mul.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void mul_op(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n *= (*stack)->n;
+
+    pop(stack, line_number); /* Remove the top element */
+}
